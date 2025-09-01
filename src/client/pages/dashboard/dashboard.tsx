4b0f7b9781c752a553fa5d24e0bc74b12df69579
clivemchd <clivemchd@gmail.com>
2025-09-01 13:59:44 +0200

    feat: add local image upload

--- src/client/pages/dashboard/dashboard.tsx
+++ src/client/pages/dashboard/dashboard.tsx
@@ -1,12 +1,33 @@
 // import { logout } from "wasp/client/auth";
 // import { generateTextToImage, generateImageToImage } from "wasp/client/operations";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 
 export const DashboardPage = () => {
+    const fileInputRef = useRef(null);
+    const [image, setimage] = useState('');
+
     return (
         <div>
-            hello world
+            <img src={image} alt="Uploaded" />
+            <input
+                type="file"
+                ref={fileInputRef}
+                onChange={(e) => {
+                    // @ts-ignore
+                    const file = e.target.files[0];
+                    if (file) {
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            // @ts-ignore
+                            setimage(reader.result);
+                        };
+                        reader.readAsDataURL(file);
+                    }
+                }}
+                // className="hidden"
+                accept="image/*"
+            />
         </div>
     )
 };
