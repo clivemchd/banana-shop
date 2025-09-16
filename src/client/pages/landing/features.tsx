0307be18f549758e9541c67ecaee11b4ac00edec
clivemchd <clivemchd@gmail.com>
2025-09-16 10:50:25 +0200

    update name to Nano Studio

--- src/client/pages/landing/features.tsx
+++ src/client/pages/landing/features.tsx
@@ -13,7 +13,7 @@ const features = [
     description:
       "Effortlessly generate high-quality, professional-grade images. Powered by Nano Banana.",
     videoSettings: {
-      src: "/public/assets/videos/short-cap-full-tut-music.mp4",
+      src: "/assets/videos/short-cap-full-tut-music.mp4",
       startTime: 0,
       endTime: 3,
       loop: true,
@@ -36,7 +36,7 @@ const features = [
     description:
       "Edit images seamlessly with advanced precision tools. Perfect for inpainting and detailed adjustments.",
     videoSettings: {
-      src: "/public/assets/videos/short-cap-full-tut-music.mp4",
+      src: "/assets/videos/short-cap-full-tut-music.mp4",
       startTime: 5.5,
       endTime: 15,
       loop: true,
@@ -59,7 +59,7 @@ const features = [
     description:
       "Easily track, undo, and redo changes. Stay in control of your creative process at all times.",
     videoSettings: {
-      src: "/public/assets/videos/cap-undo-redo-fast.mp4",
+      src: "/assets/videos/cap-undo-redo-fast.mp4",
       startTime: 0,
       endTime: 6,
       loop: true,
